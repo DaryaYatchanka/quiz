bfc5e0e2dfbc97e38665b70ef98042e268e05345
Darya Yatchanka <darya.yatchanka@gmail.com>
2024-06-06 14:11:49 -0400

    Queries added to repository
    git commit -m "user repository"

--- src/main/java/com/quiz/quiz/repositories/UserRepository.java
+++ src/main/java/com/quiz/quiz/repositories/UserRepository.java
@@ -12,12 +12,10 @@ public interface UserRepository {
     @Query(value = "SELECT * FROM user WHERE userName=?",nativeQuery = true)
     public User findByUsername(String username);
 
-
     @Query(value = "SElECT * FROM user WHERE Id=?", nativeQuery = true)
     public User findById(UUID Id);
 
     @Query(value = "SELECT * FROM user WHERE id=? AND Score=?", nativeQuery = true)
     public User findByIdAndScore(UUID Id, Integer score);
-
 
 }
