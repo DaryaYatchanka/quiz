4ec4807273f58615de86af7692094f7f5b79a294
Darya Yatchanka <darya.yatchanka@gmail.com>
2024-06-06 14:04:46 -0400

    first Quesry added to repository

--- src/main/java/com/quiz/quiz/repositories/UserRepository.java
+++ src/main/java/com/quiz/quiz/repositories/UserRepository.java
@@ -1,7 +1,12 @@
 package com.quiz.quiz.repositories;
 
+import com.quiz.quiz.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository {
+
+    @Query(value = "SELECT * FROM user WHERE userName=?",nativeQuery = true)
+    public User findByUsername(String username);
 }
