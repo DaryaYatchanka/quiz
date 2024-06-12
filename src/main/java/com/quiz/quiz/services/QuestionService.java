d72b99cfe0bed79261c308c1283291d51cf6b5ec
Darya Yatchanka <darya.yatchanka@gmail.com>
2024-06-12 17:40:03 -0400

    Question service quick touch

--- src/main/java/com/quiz/quiz/services/QuestionService.java
+++ src/main/java/com/quiz/quiz/services/QuestionService.java
@@ -1,7 +1,18 @@
 package com.quiz.quiz.services;
 
+import com.quiz.quiz.entities.Category;
+import com.quiz.quiz.entities.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    Category category;
+    ArrayList<Question> getQuestions(Integer numberOfQuestions, Category category){
+        return null;
+    }
 }
