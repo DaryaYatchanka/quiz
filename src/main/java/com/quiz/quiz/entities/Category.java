629df61bbc6840897d991a4e598dc036363e60d5
Darya Yatchanka <darya.yatchanka@gmail.com>
2024-06-12 17:05:28 -0400

    lombok added, all getters/setters/constructors remoced

--- src/main/java/com/quiz/quiz/entities/Category.java
+++ src/main/java/com/quiz/quiz/entities/Category.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
 @Entity
 public class Category {
 
@@ -15,56 +21,9 @@ public class Category {
     private String subTopic;
     private Integer level;
 
-    public Category(){}
 
-    public Category(Long id, String subject){
-        this.id= id;
-        this.subject = subject;
-    }
 
-    public Category(Long id, String subject, String subTopic){
-        this.id = id;
-        this.subject = subject;
-        this.subTopic = subTopic;
-    }
 
-    public Category(Long id, String subject, String subTopic, Integer level){
-        this.id = id;
-        this.subject = subject;
-        this.subTopic = subTopic;
-        this.level = level;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubTopic(String subTopic) {
-        this.subTopic = subTopic;
-    }
-
-    public String getSubTopic() {
-        return subTopic;
-    }
-
-    public void setLevel(Integer level) {
-        this.level = level;
-    }
-
-    public Integer getLevel() {
-        return level;
-    }
 }
 
--- src/main/java/com/quiz/quiz/entities/User.java
+++ src/main/java/com/quiz/quiz/entities/User.java
@@ -6,9 +6,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
-import java.util.UUID;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
 @Entity
 public class User {
 
@@ -24,71 +28,5 @@ public class User {
     @OneToMany
     private Integer score;
 
-
-    public User(){}
-
-    public User(Long id, String email, String password){
-        this.email= email;
-        this.id = id;
-        this.password = password;
-
-    }
-
-    public User(Long id, String userName, String userLastName, String email, String password){
-        this.id = id;
-        this.userName = userName;
-        this.userLastName = userLastName;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setUserLastName(String userLastName) {
-        this.userLastName = userLastName;
-    }
-
-    public String getUserLastName() {
-        return userLastName;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-
-    public Integer getScore() {
-        return score;
-    }
-
-    public void setScore(Integer score) {
-        this.score = score;
-    }
 
 }
