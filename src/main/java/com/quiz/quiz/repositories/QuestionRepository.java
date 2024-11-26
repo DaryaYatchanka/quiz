48f5fef0c6a6eb53a65d5e5fb61ca2d4e2878e24
Darya Yatchanka <darya.yatchanka@gmail.com>
2024-11-25 23:55:28 -0500

    come back and look at me, Dasha

--- src/main/java/com/quiz/quiz/repositories/QuestionRepository.java
+++ src/main/java/com/quiz/quiz/repositories/QuestionRepository.java
@@ -1,19 +1,34 @@
 package com.quiz.quiz.repositories;
 import com.quiz.quiz.entities.Category;
 import com.quiz.quiz.entities.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.List;
+
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+//    Page<Question> findbyCategory(Category category, Pageable pageable);
+
+    @Query("SELECT DISTINCT q.subject FROM Question q")
+    List<String> findDistinctSubject();
+    Page<Question> findBySubject(String subject, Pageable pageable);
+
+//    @Query("SELECT DISTINCT q.subject FROM Question q")
+//    List<Category> findUniqueCategory;
+
 
 //    @Query(value = "SELECT * FROM question WHERE questionId=?", nativeQuery = true)
 //    public Question findById(Long id);
 
-    @Query(value="SELECT * FROM question WHERE questionCategory=?", nativeQuery = true)
-    public Question findByCategory(Category category);
-
+//    @Query(value="SELECT * FROM question WHERE questionCategory=?", nativeQuery = true)
+//    public Question findByCategory(Category category);
+//
+//    List<Category> findDistinctCategory(Category category);
 }
--- src/main/java/com/quiz/quiz/services/QuestionService.java
+++ src/main/java/com/quiz/quiz/services/QuestionService.java
@@ -2,17 +2,122 @@ package com.quiz.quiz.services;
 
 import com.quiz.quiz.entities.Category;
 import com.quiz.quiz.entities.Question;
+import com.quiz.quiz.repositories.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+//@RequiredArgsConstructor
 @Service
 public class QuestionService {
 
+    //private final QuestionRepository questionRepository;
     @Autowired
-    Category category;
-    ArrayList<Question> getQuestions(Integer numberOfQuestions, Category category){
-        return null;
+    QuestionRepository questionRepository;
+
+//    @Autowired
+//    Category category;
+//
+//    public Question createQuestion(Question question){
+//        return questionRepository.save(question);
+//    }
+//
+//    public Optional<Question> getQuestionById(Long id){
+//        return questionRepository.findById(id);
+//    }
+//
+//
+//    public List<Question> getAllQuestions(){
+//        return questionRepository.findAll();
+//
+//    }
+//
+//    public Question updateQuestion(Long id, Question question) throws ChangeSetPersister.NotFoundException {
+//        Optional<Question> specificQuestion = this.getQuestionById(id);
+//        if(specificQuestion.isPresent()){
+//            Question updatedQuestion = specificQuestion.get();
+//            updatedQuestion.setQuestionText(question.getQuestionText());
+//            updatedQuestion.setAnswerOptions(question.getAnswerOptions());
+//            updatedQuestion.setQuestionType(question.getQuestionType());
+//            updatedQuestion.setCategory(question.getCategory());
+//            updatedQuestion.setCorrectAnswers(question.getCorrectAnswers());
+//
+//            return questionRepository.save(updatedQuestion);
+//        }else {
+//            throw new ChangeSetPersister.NotFoundException();
+//        }
+//
+//    }
+//
+//    public void deleteQuestion(Long id){
+//        questionRepository.deleteById(id);
+//    }
+//
+//    ArrayList<Question> getQuestions(Integer numberOfQuestions, Category category){
+//        return null;
+////                questionRepository.save();
+//    }
+//
+//    //    public List<Category> getAllCategories(){
+////        return questionRepository.findDistinctCategory();
+////    }
+//
+//    public List<Question> getQuestionsForUser(Integer numberOfQuestions, Category category){
+//
+//        Pageable pageable = PageRequest.of(0, numberOfQuestions);
+//        return questionRepository.findBySubject(category, pageable).getContent();
+//
+//    }
+
+
+    public Question createQuestion(Question question) {
+        return questionRepository.save(question);
     }
+
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+
+
+    public Optional<Question> getQuestionById(Long id) {
+        return questionRepository.findById(id);
+    }
+
+    public List<String> getAllSubjects() {
+        return questionRepository.findDistinctSubject();
+    }
+
+
+    public Question updateQuestion(Long id, Question question) throws ChangeSetPersister.NotFoundException {
+        Optional<Question> theQuestion = this.getQuestionById(id);
+        if (theQuestion.isPresent()) {
+            Question updatedQuestion = theQuestion.get();
+//            updatedQuestion.setQuestion(question.getQuestion());
+//            updatedQuestion.setChoices(question.getChoices());
+//            updatedQuestion.setCorrectAnswers(question.getCorrectAnswers());
+            return questionRepository.save(updatedQuestion);
+        } else {
+            throw new ChangeSetPersister.NotFoundException();
+        }
+
+    }
+
+//    public void deleteQuestion(Long id) {
+//        questionRepository.deleteById(id);
+//    }
+//
+//    public List<Question> getQuestionsForUser(Integer numOfQuestions, String subject) {
+//        Pageable pageable = PageRequest.of(0, numOfQuestions);
+//        return questionRepository.findBySubject(subject, pageable).getContent();
+//    }
+
+
 }
